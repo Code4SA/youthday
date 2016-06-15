87d8eb77fd4a58189bbe9905dd3d40d719402724
Guus Hoekman <guushoekman@gmail.com>
2016-06-15 17:24:42 +0200

    embed code

---
+++ youthday.js
@@ -0,0 +1,6 @@
+if (document.location.hostname == "localhost") {
+    var baseurl = "";
+} else {
+    var baseurl = "https://static.code4sa.org/youthday/";
+}
+document.write('<iframe src="index.html" style="width:100%; height:600px; overflow:hidden; border-width: 0px" scrolling="no"></iframe>');
