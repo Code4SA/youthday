df6d6fa7e1e7efcf3385679ae1cbeb77835a6e2a
JD Bothma <jbothma@gmail.com>
2016-06-17 10:14:32 +0200

    fix responsive embed so all's usable

--- youthday.js
+++ youthday.js
@@ -1,6 +1,13 @@
+
 if (document.location.hostname == "localhost") {
     var baseurl = "";
 } else {
     var baseurl = "https://static.code4sa.org/youthday/";
 }
-document.write('<iframe src="' + baseurl + 'index.html" style="width:100%; height:600px; overflow:hidden; border-width: 0px" scrolling="no"></iframe>');
+document.write('<div id="code4sa-embed-youthday"></div>');
+document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
+document.write("<script>var pymParent = new pym.Parent('code4sa-embed-youthday', '" + baseurl + "index.html', {});\n" +
+"pymParent.onMessage('childShrank', function(height) {\n" +
+"  pymParent.iframe.setAttribute('style', 'height: ' + height + 'px');\n" +
+"});" +
+"</script>");
