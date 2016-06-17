547c36aeb582612256e6e07a461fb7f429fb846f
JD Bothma <jbothma@gmail.com>
2016-06-17 09:41:47 +0200

    use baseurl so that embeds on other domains work

--- youthday.js
+++ youthday.js
@@ -3,4 +3,4 @@ if (document.location.hostname == "localhost") {
 } else {
     var baseurl = "https://static.code4sa.org/youthday/";
 }
-document.write('<iframe src="index.html" style="width:100%; height:600px; overflow:hidden; border-width: 0px" scrolling="no"></iframe>');
+document.write('<iframe src="' + baseurl + 'index.html" style="width:100%; height:600px; overflow:hidden; border-width: 0px" scrolling="no"></iframe>');
